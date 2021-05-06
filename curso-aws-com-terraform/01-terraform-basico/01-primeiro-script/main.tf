cc5c64a93295814f16a81a709da443cfba03cc69
Donizete Debroski <donizetedebroski@gmail.com>
2021-05-06 06:24:46 -0300

    Alteraçõs Home

--- curso-aws-com-terraform/01-terraform-basico/01-primeiro-script/main.tf
+++ curso-aws-com-terraform/01-terraform-basico/01-primeiro-script/main.tf
@@ -12,20 +12,21 @@ terraform {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 provider "aws" {
   # as credenciais podem ser inseridas manualmente ou via cli aws
-  #access_key = ""
-  #secret_key = ""
-  region  = "us-east-1"
-  profile = "tf014"
+  access_key = "AKIAQIR2NS6S7GKENXF2"
+  secret_key = "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"
+  region     = "us-east-1"
+  profile    = "tf014"
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "tf-test-donizete123"
+  bucket = "tf-test-donizete"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
     Managedby   = "Terraform"
+    Cliente     = "Debroski"
   }
 }
